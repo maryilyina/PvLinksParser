34efdc12a05df0fdfe885f4ef4c924360b5c5b95
maryilyina <maryilyina96@gmail.com>
2017-09-15 16:35:37 +0700

    initial commit

---
+++ src/PvLinksParser.java
@@ -0,0 +1,13 @@
+public class PvLinksParser {
+
+    private PvLinksParser(){
+
+    }
+
+    public static void main(String[] args) {
+        PvLinksParser pvLinksParser = new PvLinksParser();
+    }
+
+}
+
+
